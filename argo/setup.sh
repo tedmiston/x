0249ab087d8a13ace2a86281c0e990bef4bfec54
Taylor D. Edmiston <tedmiston@gmail.com>
2019-09-22 20:11:10 -0400

    Move setup commands to script

---
+++ argo/setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo curl -sfL https://get.k3s.io | sh -
+sudo systemctl status k3s
+sudo k3s kubectl --version
+sudo systemctl stop k3s
+echo "running k3s command"
+sudo k3s server &
+sudo k3s kubectl get nodes
+sudo kubectl get nodes
+sudo k3s kubectl get pods --all-namespaces
+sudo kubectl get pods --all-namespaces
