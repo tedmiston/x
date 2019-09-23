6b4b8e712dda81e6f09cc68f8c5839be17abe4d9
Taylor D. Edmiston <tedmiston@gmail.com>
2019-09-22 20:32:24 -0400

    Use docker

--- argo/setup.sh
+++ argo/setup.sh
@@ -7,7 +7,7 @@ sudo systemctl status k3s
 sudo k3s kubectl --version
 sudo systemctl stop k3s
 echo "running k3s command"
-sudo k3s server --disable-agent &
+sudo k3s server --docker --disable-agent &
 sudo k3s kubectl get nodes
 sudo kubectl get nodes
 sudo k3s kubectl get pods --all-namespaces
