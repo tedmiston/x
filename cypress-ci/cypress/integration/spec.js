0c35714e7d36bf29eed5ff88747c1cad7d9eb53c
Taylor D. Edmiston <tedmiston@gmail.com>
2020-01-02 21:20:34 -0500

    Clean up

--- cypress-ci/cypress/integration/spec.js
+++ cypress-ci/cypress/integration/spec.js
@@ -1,18 +1,6 @@
-describe('My First Test', function() {
-  it('Does not do much!', function() {
-    expect(true).to.equal(true)
-  })
-})
-
-describe('My Second Test', function() {
+describe('First Test', function() {
   it('Visits the Homepage', function() {
-    cy.visit('http://localhost:5000/')
+    cy.visit('http://localhost:5000')
     cy.contains('This is a test')
   })
 })
-
-// describe('Another One', function() {
-//   it('This should break', function() {
-//     expect(true).to.equal(false)
-//   })
-// })
